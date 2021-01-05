127e9aefab57e5d819ee5695d01f0c201c43a53c
vitorrafael <vrsilveira07@gmail.com>
2021-01-05 17:04:01 -0300

    Add error handling

--- srv/book-service.js
+++ srv/book-service.js
@@ -1,10 +1,17 @@
 const AuthorService = require("./services/AuthorService");
 const BookService = require("./services/BookService");
 
+const log = require("cf-nodejs-logging-support");
+
 module.exports = function(server) {
 
     server.before("CREATE", "Books", async req => { 
-        await BookService.CreateBook(req);
+        try {
+            await BookService.CreateBook(req);
+        } catch(error) {
+            log.error((error && error.stack) ? error.stack : error);
+            req.reject(error.status || 500, error);
+        }
     });
 
     server.before("CREATE", "Authors", async req => {
